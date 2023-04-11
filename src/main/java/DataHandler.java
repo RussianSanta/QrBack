25ea3525fb1c154e5bf3faae4078727dd67fa284
Maks <zheltov2001@gmail.com>
2023-04-11 18:03:19 +0300

    Добавлена техническая информация в блоки для проверки при расшифровке

--- src/main/java/DataHandler.java
+++ src/main/java/DataHandler.java
@@ -8,18 +8,24 @@ import org.jcodec.scale.AWTUtil;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DataHandler {
+    private static String fileExtension = "";
+
     public static void toMyFormat(File file) throws IOException {
         ArrayList<String> dataSets = prepareData(file);
         ArrayList<BufferedImage> images = convertDataToImages(dataSets);
         collectToVideo(images);
+        System.out.println("Успешно зашифровано. Полученный файл можно найти в папке result");
     }
 
     public static void fromMyFormat(File file) throws JCodecException, IOException {
         ArrayList<BufferedImage> images = cutTheVideo(file);
         ArrayList<String> dataSets = convertImagesToData(images);
         collectToFile(dataSets);
+        System.out.println("Успешно расшифровано. Полученный файл можно найти в папке resources");
     }
 
     private static ArrayList<String> prepareData(File file) {
@@ -45,7 +51,7 @@ public class DataHandler {
             dataSets.add(String.copyValueOf(chars));
             counter += blockSize;
         }
-        return dataSets;
+        return appendTechnicalData(dataSets, file.getName());
     }
 
     private static ArrayList<BufferedImage> convertDataToImages(ArrayList<String> dataSets) {
@@ -84,27 +90,59 @@ public class DataHandler {
         return images;
     }
 
-    private static ArrayList<String> convertImagesToData(ArrayList<BufferedImage> images) {
+    private static ArrayList<String> convertImagesToData(ArrayList<BufferedImage> images) throws IOException {
         ArrayList<String> dataSets = new ArrayList<>();
+        Set<Integer> collectedDataSets = new HashSet<>();
+        int countOfDataSets = -1;
+        int numberOfDataSet = -1;
 
         for (BufferedImage image : images) {
             String decodeResult = QrProvider.decode(image).getText();
+            countOfDataSets = Integer.parseInt(decodeResult.substring(0, 6));
+            fileExtension = decodeResult.substring(6, 12);
+            fileExtension = fileExtension.replaceAll(" ", "");
+            numberOfDataSet = Integer.parseInt(decodeResult.substring(12, 18));
+            collectedDataSets.add(numberOfDataSet);
             dataSets.add(decodeResult);
         }
 
+        if (countOfDataSets != collectedDataSets.size()) {
+            throw new IOException("Ошибка при получении данных. Количество полученных блоков не совпадает с ожидаемым");
+        }
         return dataSets;
     }
 
     private static void collectToFile(ArrayList<String> dataSets) {
         StringBuilder builder = new StringBuilder();
         for (String s : dataSets) {
+            s = s.substring(18);
             builder.append(s);
         }
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/resources/result.txt"))) {
+        String resultFileName = "src/main/resources/result." + fileExtension;
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(resultFileName))) {
             bw.write(builder.toString());
         } catch (IOException ex) {
 
             System.out.println(ex.getMessage());
         }
+    }
+
+    private static ArrayList<String> appendTechnicalData(ArrayList<String> dataSets, String coreFileName) {
+        ArrayList<String> newDataSets = new ArrayList<>();
+        int countOfFrames = dataSets.size();
+        int dotPosition = coreFileName.indexOf(".") + 1;
+        String fileExtension = coreFileName.substring(dotPosition);
+        StringBuilder technicalDataBuilder = new StringBuilder();
+        technicalDataBuilder
+                .append("0".repeat(Math.max(0, 6 - String.valueOf(countOfFrames).length())))
+                .append(countOfFrames)
+                .append(" ".repeat(Math.max(0, 6 - fileExtension.length())))
+                .append(fileExtension);
+        for (String s : dataSets) {
+            String dataSetNumber = "0".repeat(Math.max(0, 6 - String.valueOf(dataSets.indexOf(s)).length())) + dataSets.indexOf(s);
+            s = technicalDataBuilder + dataSetNumber + s;
+            newDataSets.add(s);
+        }
+        return newDataSets;
     }
 }
